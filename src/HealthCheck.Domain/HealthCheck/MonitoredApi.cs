34eafc3eb81a7283c352b4d2db1c0cc2f9997e4c
itsluucasdev <lucas.bcsantos@outlook.com>
2024-05-25 05:48:26 -0300

    - initial commit - ddd solution tree folders - application / domain / infra / utils - git ignore files

---
+++ src/HealthCheck.Domain/HealthCheck/MonitoredApi.cs
@@ -0,0 +1,10 @@
+﻿namespace HealthCheck.Domain.HealthCheck;
+
+public class MonitoredApi
+{
+    public string Hash { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Details { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
