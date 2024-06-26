c12ded7f9d66f70cbbfa3fd2f52e1eb697e528e0
itsluucasdev <lucas.bcsantos@outlook.com>
2024-06-25 23:21:00 -0300

    - changing test framework from xUnit to MSTest - create a configuration builder - formatting generic api response

---
+++ src/HealthCheck.Utils/AWS/ServiceCollectionExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HealthCheck.Utils.AWS;
+
+public static class ServiceCollectionExtension
+{
+    public static Tuple<IServiceCollection, IConfiguration> CreateConfig(this IServiceCollection sc)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+#if DEBUG
+            .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+#elif RELEASE
+            .AddJsonFile($"appsettings.json", optional: true, reloadOnChange: true)
+#endif
+            .Build();
+
+        return new Tuple<IServiceCollection, IConfiguration>(sc, configuration);
+    }
+}
